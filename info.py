e9a6c8155c679d15908f123be47d720a0a3c9525
hitesh-123-teca <hiteshrathiya123@gmail.com>
2025-02-05 00:40:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', '21688431'))
+API_HASH = environ.get('API_HASH', 'db274cb8e9167e731d9c8305197badeb')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7434864670:AAG-pcPyQIrkLmBl966ee57mN2CSBHf9BOk')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5665677555').split()]
